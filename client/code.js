39673f82f16eb9ec5d23c1bc041aff6bfb4e079c
TobiasOnandia <tobiasonandia0@gmail.com>
2024-03-29 20:12:19 -0300

    styled

--- client/code.js
+++ client/code.js
@@ -3,7 +3,7 @@ import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js'
 const socket = io()
 
 const form = document.getElementById("form")
-const input = document.getElementById("input")
+const input = document.getElementById("MsgInput")
 const messages = document.getElementById("messages")
 
 socket.on("chat message", (msg) => {
