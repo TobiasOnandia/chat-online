774493c3b47108a21aeaa4be0f9c74c9f7723da8
TobiasOnandia <tobiasonandia0@gmail.com>
2024-04-01 15:29:52 -0300

    integration username

--- client/code.js
+++ client/code.js
@@ -1,9 +1,24 @@
 import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js'
 
+
+const getUsername = async () => {
+    const username = localStorage.getItem('username')
+    if(username){
+        return username
+    }
+
+    const res = await fetch('https://random-data-api.com/api/users/random_user')
+    const { username: randomUsername } = await res.json()
+
+    localStorage.setItem('username', randomUsername)
+    return randomUsername
+}
+
+
 const socket = io({
     auth: {
         serverOffset: 0,
-        username: 'tobias'
+        username: await getUsername()
     }
 })
 
@@ -12,10 +27,22 @@ const input = document.getElementById("MsgInput")
 const messages = document.getElementById("messages")
 const nameUser = document.querySelector(".user")
 
-socket.on("chat message", (msg, serverOffset) => {
-    const item = `<li>${msg}</li>`
+
+
+
+
+socket.on("chat message", (msg, serverOffset, username) => {
+    const item = `<li>
+    <p>${msg}</p>
+    <small>${username} </small>
+    </li>`
     messages.insertAdjacentHTML("beforeend", item)
     socket.auth.serverOffset = serverOffset
+
+    //controlar el scroll
+
+    messages.scrollTop = messages.scrollHeight
+
 })
 
 form.addEventListener("submit", (e) =>{
